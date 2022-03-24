a71fe732e5fe6799c22c1c29d220e4df84c2cba0
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2022-03-23 22:07:27 -0300

    Add base for model reading

--- Hello3D/Exericio8/Origem.cpp
+++ Hello3D/Exericio8/Origem.cpp
@@ -1,5 +1,11 @@
 ﻿using namespace std;
 
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include "Program.cpp"
+#include "Shader.cpp"
+#include "Model.cpp"
+
 #include <string>
 #include <assert.h>
 #include <glad/glad.h>
@@ -7,9 +13,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
-#include "Program.cpp"
-#include "Shader.cpp"
 
 // Protótipo da função de callback de teclado
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -27,6 +30,7 @@ Program setupProgram()
 // Protótipos das funções
 int setupGeometry();
 int setupFloorGeometry();
+int setupModelGeometry();
 
 const GLuint WIDTH = 1000, HEIGHT = 1000;
 
@@ -82,6 +86,7 @@ int main()
 	// Gerando um buffer simples, com a geometria de um triângulo
 	GLuint VAO = setupGeometry();
 	GLuint VAO_FLOOR = setupFloorGeometry();
+	GLuint VAO_MODEL = setupModelGeometry();
 
 	glUseProgram(program.GetProgram());
 
@@ -154,18 +159,21 @@ int main()
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		glBindVertexArray(VAO);
-		glBindVertexArray(VAO_FLOOR);
+		//glBindVertexArray(VAO);
+		//glBindVertexArray(VAO_FLOOR);
+		glBindVertexArray(VAO_MODEL);
 
 		//FOCUS: This dictates how many figures will be drawn
-		drawElement(VAO, 12);
-		drawElement(VAO_FLOOR, 2);
+		//drawElement(VAO, 12);
+		//drawElement(VAO_FLOOR, 2);
+		drawElement(VAO_MODEL, 12);
 
 		// Troca os buffers da tela
 		glfwSwapBuffers(window);
 	}
 	// Pede pra OpenGL desalocar os buffers
-	glDeleteVertexArrays(1, &VAO);
+	//glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &VAO_MODEL);
 
 	glfwTerminate();
 	return 0;
@@ -522,6 +530,73 @@ int setupFloorGeometry()
 	glEnableVertexAttribArray(0);
 
 	//Atributo cor (r, g, b)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+
+	return VAO;
+}
+int setupModelGeometry() {
+
+	Model threeDimensionalModel = Model("Resources/Models/Cube/cube.obj");
+
+	printf("Number of vertices %d | Number of triangles %d | Number of squares %d \n",
+		threeDimensionalModel.getVertices().size(),
+		threeDimensionalModel.getVertices().size() / 3,
+		threeDimensionalModel.getVertices().size() / 6);
+
+	for (int i = 0; i < threeDimensionalModel.getVertices().size(); ++i) {
+		printf("Vertice x: %f y: %f z: %f \n", threeDimensionalModel.getVertices()[i].x, threeDimensionalModel.getVertices()[i].y, threeDimensionalModel.getVertices()[i].z);
+	}
+
+	for (int i = 0; i < threeDimensionalModel.getUvs().size(); ++i) {
+		printf("Uvs x: %f y: %f  \n", threeDimensionalModel.getUvs()[i].x, threeDimensionalModel.getUvs()[i].y);
+	}
+
+	for (int i = 0; i < threeDimensionalModel.getNormals().size(); ++i) {
+		printf("Normal x: %f y: %f z: %f  \n", threeDimensionalModel.getNormals()[i].x, threeDimensionalModel.getNormals()[i].y, threeDimensionalModel.getNormals()[i].z);
+	}
+
+	GLuint VBO, VAO;
+
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//Envia os dados do array de floats para o buffer da OpenGl
+	//glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, threeDimensionalModel.getVertices().size() * sizeof(glm::vec3), &threeDimensionalModel.getVertices()[0], GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(floor), floor, GL_STATIC_DRAW);
+
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se está normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+
+	//Atributo posição (x, y, z)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	////Atributo cor (r, g, b)
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
