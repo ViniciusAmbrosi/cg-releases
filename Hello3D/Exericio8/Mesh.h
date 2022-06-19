5b249ecabd6bc470526a0fea6d0e5a4e47e195cd
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2022-06-19 17:42:42 -0300

    Cleanup

--- Hello3D/Exericio8/Mesh.h
+++ Hello3D/Exericio8/Mesh.h
@@ -18,10 +18,6 @@ struct Vertex {
     glm::vec3 Position;
     glm::vec3 Normal;
     glm::vec2 TexCoords;
-    glm::vec3 Tangent;
-    glm::vec3 Bitangent;
-    int m_BoneIDs[MAX_BONE_INFLUENCE];
-    float m_Weights[MAX_BONE_INFLUENCE];
 };
 
 struct Texture {
@@ -32,8 +28,8 @@ struct Texture {
 
 class Mesh {
 public:
-    vector<Vertex>       vertices;
-    vector<Texture>      textures;
+    vector<Vertex> vertices;
+    vector<Texture> textures;
 
     vector<unsigned int> indices;
 
@@ -70,9 +66,7 @@ public:
             else if (name == "texture_height")
                 number = std::to_string(heightNr++); // transfer unsigned int to string
 
-            // now set the sampler to the correct texture unit
             glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
-            // and finally bind the texture
             glBindTexture(GL_TEXTURE_2D, textures[i].id);
         }
 
@@ -100,9 +94,6 @@ private:
         glBindVertexArray(VAO);
         // load data into vertex buffers
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // A great thing about structs is that their memory layout is sequential for all its items.
-        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-        // again translates to 3/2 floats which translates to a byte array.
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -118,23 +109,6 @@ private:
         // vertex texture coords
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
-
-        // Valores não necessários até o momento -------------------------------------
-
-        //// vertex tangent
-        //glEnableVertexAttribArray(3);
-        //glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
-        //// vertex bitangent
-        //glEnableVertexAttribArray(4);
-        //glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
-        //// ids
-        //glEnableVertexAttribArray(5);
-        //glVertexAttribIPointer(5, 4, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, m_BoneIDs));
-        // weights
-        //glEnableVertexAttribArray(6);
-        //glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_Weights));
-
-        // Valores não necessários até o momento -------------------------------------
 
         glBindVertexArray(0);
     }
